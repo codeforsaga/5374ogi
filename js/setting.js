5dfb2ba29c9fb4f61112804364db05b0d629e7c5
Yoshiaki Hori <hori@hori-s.net>
2018-06-23 17:28:13 +0900

    Initialize repository

---
+++ js/setting.js
@@ -0,0 +1,12 @@
+/*
+ * 5374 setting
+ *
+*/
+
+var SVGLabel = false; // SVGイメージを使用するときは、true。用意できない場合はfalse。
+
+var MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+
+var MaxMonth = 8; // 何ヶ月先まで計算するかを指定します。
+
+var WeekShift = true; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
